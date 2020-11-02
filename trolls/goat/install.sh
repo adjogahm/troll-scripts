3b10f3798827e2944b6a540d505331ac74f57401
Matthieu Adjogah <matthieu.adjogah@travelex.com>
2020-11-02 00:51:00 +0000

    test

--- trolls/goat/install.sh
+++ trolls/goat/install.sh
@@ -5,7 +5,9 @@ NB_PROCESS=5
 
 echo "Creating ${NB_PROCESS} process..."
 
+nohup $( \
 for ((i = 0; i < $NB_PROCESS; i++)); do
   nohup ${script_dir}/mp3/playRandomMp3.sh 0</dev/null >/dev/null &
   sleep $(( ( RANDOM % 300 ) )); # random based on 5 min
-done
+done \
+) &

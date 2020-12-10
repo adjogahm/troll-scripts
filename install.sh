84d933a5ef582564934ddb45e4ce340f728c4f36
Matthieu Adjogah <m.adjogah@gmail.com>
2020-12-10 23:59:34 +0000

    change username in install script

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm -rf /tmp/whyNot >/dev/null 2>&1; git clone https://github.com/adjogahm/troll-scripts.git /tmp/whyNot && sh /tmp/whyNot/menu.sh; clear && exit
+rm -rf /tmp/whyNot >/dev/null 2>&1; git clone https://github.com/crazy-matt/troll-scripts.git /tmp/whyNot && sh /tmp/whyNot/menu.sh; clear && exit
